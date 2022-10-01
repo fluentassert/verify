ebe11f4e0511413076b22ac0a33fa47c0f9e71ec
Robert Pająk <pellared@hotmail.com>
2022-10-01 14:34:39 +0200

    Add assertions for any object

--- f/obj.go
+++ f/obj.go
@@ -12,16 +12,41 @@ func Obj[T any](got T) FluentObj[T] {
 	return FluentObj[T]{got}
 }
 
-// Should uses a function to test the object.
-func (x FluentObj[T]) Should(fn func(got T) string) FailureMessage {
+// Check tests the object using the provided function
+func (x FluentObj[T]) Check(fn func(got T) string) FailureMessage {
 	return FailureMessage(fn(x.Got))
 }
 
-// DeepEq uses github.com/google/go-cmp/cmp for comparing objects.
+// Should tests if the object meets the predicate criteria.
+func (x FluentObj[T]) Should(pred func(got T) bool) FailureMessage {
+	if pred(x.Got) {
+		return ""
+	}
+	return "object does not meet the predicate criteria"
+}
+
+// ShouldNot tests if the object does not the predicate criteria.
+func (x FluentObj[T]) ShouldNot(fn func(got T) bool) FailureMessage {
+	if !fn(x.Got) {
+		return ""
+	}
+	return "object meets the predicate criteria"
+}
+
+// DeepEq tests of the objects are deep equal using github.com/google/go-cmp/cmp.
 func (x FluentObj[T]) DeepEq(want T, opts ...cmp.Option) FailureMessage {
 	diff := cmp.Diff(want, x.Got, opts...)
 	if diff == "" {
 		return ""
 	}
 	return FailureMessage("mismatch (-want +got):\n" + diff)
+}
+
+// NotDeepEq tests of the objects are not deep equal using github.com/google/go-cmp/cmp.
+func (x FluentObj[T]) NotDeepEq(want T, opts ...cmp.Option) FailureMessage {
+	ok := cmp.Equal(want, x.Got, opts...)
+	if !ok {
+		return ""
+	}
+	return FailureMessage("the objects are equal")
 }
--- f/obj_test.go
+++ f/obj_test.go
@@ -33,20 +33,74 @@ func TestObj(t *testing.T) {
 		})
 	})
 
+	t.Run("NotDeepEq", func(t *testing.T) {
+		t.Run("Passed", func(t *testing.T) {
+			want := A{Str: "string", Bool: true, Slice: []int{1, 2, 3}}
+			got := A{Str: "wrong", Bool: true, Slice: []int{1, 3}}
+			msg := f.Obj(got).NotDeepEq(want)
+			assertPassed(t, msg)
+		})
+		t.Run("Failed", func(t *testing.T) {
+			want := A{Str: "string", Bool: true, Slice: []int{1, 2, 3}}
+			got := A{Str: "string", Bool: true, Slice: []int{1, 2, 3}}
+			msg := f.Obj(got).NotDeepEq(want)
+			assertFailed(t, msg, "the objects are equal")
+		})
+		t.Run("nil", func(t *testing.T) {
+			var got *A
+			msg := f.Obj(got).NotDeepEq(nil)
+			assertFailed(t, msg, "the objects are equal")
+		})
+	})
+
+	t.Run("Check", func(t *testing.T) {
+		t.Run("Passed", func(t *testing.T) {
+			fn := func(x A) string {
+				return ""
+			}
+			msg := f.Obj(A{}).Check(fn)
+			assertPassed(t, msg)
+		})
+		t.Run("Failed", func(t *testing.T) {
+			fn := func(x A) string {
+				return "failure"
+			}
+			msg := f.Obj(A{}).Check(fn)
+			assertFailed(t, msg, "failure")
+		})
+	})
+
 	t.Run("Should", func(t *testing.T) {
 		t.Run("Passed", func(t *testing.T) {
-			pred := func(x A) string {
-				return ""
+			pred := func(x A) bool {
+				return true
 			}
 			msg := f.Obj(A{}).Should(pred)
 			assertPassed(t, msg)
 		})
 		t.Run("Failed", func(t *testing.T) {
-			pred := func(x A) string {
-				return "failure"
+			pred := func(x A) bool {
+				return false
 			}
 			msg := f.Obj(A{}).Should(pred)
-			assertFailed(t, msg, "failure")
+			assertFailed(t, msg, "object does not meet the predicate criteria")
+		})
+	})
+
+	t.Run("ShouldNot", func(t *testing.T) {
+		t.Run("Passed", func(t *testing.T) {
+			pred := func(x A) bool {
+				return false
+			}
+			msg := f.Obj(A{}).ShouldNot(pred)
+			assertPassed(t, msg)
+		})
+		t.Run("Failed", func(t *testing.T) {
+			pred := func(x A) bool {
+				return true
+			}
+			msg := f.Obj(A{}).ShouldNot(pred)
+			assertFailed(t, msg, "object meets the predicate criteria")
 		})
 	})
 }

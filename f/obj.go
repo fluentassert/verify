a7a550b031fbe1d02e46e442056bc5822ef32548
Robert Pająk <pellared@hotmail.com>
2022-10-01 13:59:55 +0200

    Add missing comments

--- f/obj.go
+++ f/obj.go
@@ -2,18 +2,22 @@ package f
 
 import "github.com/google/go-cmp/cmp"
 
+// FluentObj encapsulates assertions for any objet.
 type FluentObj[T any] struct {
 	Got T
 }
 
+// Obj is used for testing any object.
 func Obj[T any](got T) FluentObj[T] {
 	return FluentObj[T]{got}
 }
 
-func (x FluentObj[T]) Should(predicate func(got T) string) FailureMessage {
-	return FailureMessage(predicate(x.Got))
+// Should uses a function to test the object.
+func (x FluentObj[T]) Should(fn func(got T) string) FailureMessage {
+	return FailureMessage(fn(x.Got))
 }
 
+// DeepEq uses github.com/google/go-cmp/cmp for comparing objects.
 func (x FluentObj[T]) DeepEq(want T) FailureMessage {
 	diff := cmp.Diff(want, x.Got)
 	if diff == "" {

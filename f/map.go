9153434241ae1e4639fe3126eb61fb1dd0ff6e34
Robert Pająk <pellared@hotmail.com>
2022-10-02 16:16:45 +0200

    Add TODO comments

--- f/map.go
+++ f/map.go
@@ -2,9 +2,17 @@ package f
 
 // TODO: type embed any
 
-// TODO: Contain
+// TODO: Contain(elements map[K]V) FailureMessage
 
-// TODO: NotContain
+// TODO: NotContain(elements map[K]V) FailureMessage
+
+// TODO: ContainKeys(keys ...K) FailureMessage
+
+// TODO: NotContainKeys(keys ...K) FailureMessage
+
+// TODO: ContainValues(values ...V) FailureMessage
+
+// TODO: NotContainValues(values ...V) FailureMessage
 
 // TODO: All(fn func(k K, v V) FailureMessage) FailureMessage
 
--- f/slice.go
+++ f/slice.go
@@ -2,9 +2,9 @@ package f
 
 // TODO: type embed any
 
-// TODO: Contain
+// TODO: Contain(elements ...T) FailureMessage
 
-// TODO: NotContain
+// TODO: NotContain(elements ...T) FailureMessage
 
 // TODO: All(fn func(elem T) FailureMessage) FailureMessage
 

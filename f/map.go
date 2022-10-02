fb4367d102dae431553c27bf83c86ff537854448
Robert Pająk <pellared@hotmail.com>
2022-10-02 16:11:28 +0200

    Prepare TODO comments for map and slice

---
+++ f/map.go
@@ -0,0 +1,11 @@
+package f
+
+// TODO: type embed any
+
+// TODO: Contain
+
+// TODO: NotContain
+
+// TODO: All(fn func(k K, v V) FailureMessage) FailureMessage
+
+// TODO: Any(fn func(k K, v V) FailureMessage) FailureMessage
---
+++ f/slice.go
@@ -0,0 +1,11 @@
+package f
+
+// TODO: type embed any
+
+// TODO: Contain
+
+// TODO: NotContain
+
+// TODO: All(fn func(elem T) FailureMessage) FailureMessage
+
+// TODO: Any(fn func(elem T) FailureMessage) FailureMessage

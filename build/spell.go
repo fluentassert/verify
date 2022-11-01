622eba9ad9300d6f01ccf3965a449124e9671330
Robert Pająk <pellared@hotmail.com>
2022-11-01 15:53:53 +0100

    Use github.com/goyek/x

--- build/spell.go
+++ build/spell.go
@@ -4,19 +4,20 @@ import (
 	"strings"
 
 	"github.com/goyek/goyek/v2"
+	"github.com/goyek/x/cmd"
 )
 
 var spell = goyek.Define(goyek.Task{
 	Name:  "spell",
 	Usage: "misspell",
 	Action: func(tf *goyek.TF) {
-		if !Exec(tf, dirBuild, "go install github.com/client9/misspell/cmd/misspell") {
+		if !cmd.Exec(tf, "go install github.com/client9/misspell/cmd/misspell", cmd.Dir(dirBuild)) {
 			return
 		}
 		mdFiles := find(tf, ".md")
 		if len(mdFiles) == 0 {
 			tf.Skip("no .md files")
 		}
-		Exec(tf, dirRoot, "misspell -error -locale=US -w "+strings.Join(mdFiles, " "))
+		cmd.Exec(tf, "misspell -error -locale=US -w "+strings.Join(mdFiles, " "))
 	},
 })
--- build/test.go
+++ build/test.go
@@ -1,14 +1,17 @@
 package main
 
-import "github.com/goyek/goyek/v2"
+import (
+	"github.com/goyek/goyek/v2"
+	"github.com/goyek/x/cmd"
+)
 
 var test = goyek.Define(goyek.Task{
 	Name:  "test",
 	Usage: "go test",
 	Action: func(tf *goyek.TF) {
-		if !Exec(tf, dirRoot, "go test -race -covermode=atomic -coverprofile=coverage.out -coverpkg=./... ./...") {
+		if !cmd.Exec(tf, "go test -race -covermode=atomic -coverprofile=coverage.out -coverpkg=./... ./...") {
 			return
 		}
-		Exec(tf, dirRoot, "go tool cover -html=coverage.out -o coverage.html")
+		cmd.Exec(tf, "go tool cover -html=coverage.out -o coverage.html")
 	},
 })

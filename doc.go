2106871c7dcaa04eace78a40d5e5197359512d73
Robert Pająk <pellared@hotmail.com>
2025-02-17 20:18:26 +0100

    Update doc.go

--- doc.go
+++ doc.go
@@ -5,4 +5,7 @@
 //
 // At last, you may embed a Fluent* type
 // to extend it with additional assertion functions.
+//
+// Deprecated: avoid using assertion libraries.
+// Use the standard library and [github.com/google/go-cmp/cmp] instead.
 package verify
